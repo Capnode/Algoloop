eaf4fb7286325fddde4c8e6128774087f52a0fed
carlcnx <carlcnx@outlook.com>
2020-05-01 12:47:29 +0200

    Update spalsh screen

--- Algoloop/Properties/Resources.Designer.cs
+++ Algoloop/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace Algoloop.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap AlgoloopSplash {
+            get {
+                object obj = ResourceManager.GetObject("AlgoloopSplash", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Checklist {
             get {
                 object obj = ResourceManager.GetObject("Checklist", resourceCulture);
