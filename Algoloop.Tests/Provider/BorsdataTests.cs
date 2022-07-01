6ad6765e24f406d1995aaec2e9c15c0be599743c
carlcnx <carlcnx@outlook.com>
2022-07-01 20:04:44 +0200

    Add provider Borsdata

---
+++ Algoloop.Tests/Provider/BorsdataTests.cs
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2019 Capnode AB
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Algoloop.Model;
+using Algoloop.ViewModel.Internal.Provider;
+using AlgoloopTests.TestSupport;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuantConnect;
+using QuantConnect.Logging;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Algoloop.Tests.Provider
+{
+    [TestClass]
+    public class BorsdataTests
+    {
+        private ProviderModel _market;
+        private SettingModel _settings;
+        private string _equityFolder;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Log.LogHandler = new ConsoleLogHandler();
+
+            string dataFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
+            _equityFolder = Path.Combine(dataFolder, SecurityType.Equity.SecurityTypeToLower(), "borsdata");
+            if (Directory.Exists(dataFolder))
+            {
+                Directory.Delete(dataFolder, true);
+            }
+
+            IConfigurationRoot settings = TestConfig.Create();
+            _market = new ProviderModel
+            {
+                Active = true,
+                Name = "Borsdata",
+                Provider = "borsdata",
+                ApiKey = settings["borsdata"],
+                LastDate = new DateTime(2021, 1, 5),
+                Resolution = Resolution.Daily
+            };
+
+            _settings = new SettingModel { DataFolder = dataFolder };
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+        }
+
+        [TestMethod]
+        public void Download_one_symbol()
+        {
+            // Arrange
+            DateTime lastDate = _market.LastDate;
+            const string ticker = "INVE-B.ST";
+            var symbol0 = new SymbolModel(ticker, string.Empty, SecurityType.Base)
+            {
+                Active = false,
+                Name = ticker
+            };
+            _market.Symbols.Add(symbol0);
+
+            // Act
+            using IProvider provider = ProviderFactory.CreateProvider(_market.Provider, _settings);
+            provider.GetUpdate(_market, null);
+            SymbolModel symbol = _market.Symbols.SingleOrDefault();
+
+            // Assert
+            Assert.IsNotNull(symbol);
+            Assert.AreSame(symbol0, symbol);
+            Assert.IsFalse(_market.Active);
+            Assert.IsTrue(_market.LastDate > lastDate);
+            Assert.AreEqual(symbol0.Id, symbol.Id);
+            Assert.AreNotEqual(symbol.Id, symbol.Name);
+            Assert.AreEqual(symbol0.Active, symbol.Active);
+            Assert.AreEqual("Investor B", symbol.Name);
+            Assert.AreEqual(SecurityType.Equity, symbol.Security);
+            Assert.AreEqual("borsdata", symbol.Market);
+            Assert.IsNotNull(symbol.Properties);
+            Assert.AreEqual(5, symbol.Properties.Count);
+            Assert.IsTrue(File.Exists(Path.Combine(_equityFolder, "daily", "inve-b.st.zip")));
+        }
+    }
+}
---
+++ Algoloop.ViewModel/Internal/Provider/Borsdata.cs
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2019 Capnode AB
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Algoloop.Model;
+using Algoloop.ToolBox.Borsdata;
+using QuantConnect;
+using QuantConnect.Securities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.Linq;
+
+namespace Algoloop.ViewModel.Internal.Provider
+{
+    internal class Borsdata : ProviderBase
+    {
+        private SettingModel _settings;
+
+        public override bool Register(SettingModel settings)
+        {
+            Contract.Requires(settings != null, nameof(settings));
+            if (!base.Register(settings)) return false;
+
+            _settings = settings;
+            string providerId = GetType().Name.ToLowerInvariant();
+            BorsdataDownloaderProgram.Register(settings.DataFolder, providerId);
+            return true;
+        }
+
+        public override void GetUpdate(ProviderModel market, Action<object> update)
+        {
+            if (market == null) throw new ArgumentNullException(nameof(market));
+            if (!market.Resolution.Equals(Resolution.Daily)) throw new ArgumentException(nameof(market.Resolution));
+
+            // Update symbol list
+            using var downloader = new BorsdataDataDownloader(market.ApiKey);
+            IEnumerable<SymbolModel> actual = downloader.GetInstruments();
+            bool addNew = market.Symbols.Count != 1; // For test
+            UpdateSymbols(market, actual, addNew);
+
+            // Setup download parameters
+            IList<string> symbols = market.Symbols.Select(m => m.Id).ToList();
+            DateTime utsNow = DateTime.UtcNow;
+            string from = market.LastDate.ToUniversalTime().ToString("yyyyMMdd-HH:mm:ss", CultureInfo.InvariantCulture);
+            string to = utsNow.ToString("yyyyMMdd-HH:mm:ss", CultureInfo.InvariantCulture);
+            string[] args =
+            {
+                "--app=BorsdataDownloader",
+                $"--from-date={from}",
+                $"--to-date={to}",
+                $"--api-key={market.ApiKey}",
+                $"--tickers={string.Join(",", symbols)}"
+            };
+            IDictionary<string, string> config = new Dictionary<string, string>
+            {
+                ["data-directory"] = _settings.DataFolder,
+                ["cache-location"] = _settings.DataFolder,
+                ["data-folder"] = _settings.DataFolder
+            };
+
+            // Download active symbols
+            RunProcess("Algoloop.ToolBox.exe", args, config);
+            market.LastDate = utsNow.ToLocalTime();
+            market.Active = false;
+        }
+    }
+}
