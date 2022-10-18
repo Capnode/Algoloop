7ecfb4fa511bf289f1d207526dcc63224b381fc7
carlcnx <carlcnx@outlook.com>
2022-10-18 23:13:55 +0200

    Fix today is not run in backtest

--- Algorithm/QCAlgorithm.cs
+++ Algorithm/QCAlgorithm.cs
@@ -1514,11 +1514,11 @@ namespace QuantConnect.Algorithm
 
             //Validate:
             //2. Check Range:
-            var yesterdayInAlgorithmTimeZone = DateTime.UtcNow.ConvertFromUtc(TimeZone).Date.AddDays(-1);
-            if (end > yesterdayInAlgorithmTimeZone)
-            {
-                end = yesterdayInAlgorithmTimeZone;
-            }
+            //var yesterdayInAlgorithmTimeZone = DateTime.UtcNow.ConvertFromUtc(TimeZone).Date.AddDays(-1);
+            //if (end > yesterdayInAlgorithmTimeZone)
+            //{
+            //    end = yesterdayInAlgorithmTimeZone;
+            //}
 
             //3. Make this at the very end of the requested date
             _endDate = end.RoundDown(TimeSpan.FromDays(1)).AddDays(1).AddTicks(-1);
