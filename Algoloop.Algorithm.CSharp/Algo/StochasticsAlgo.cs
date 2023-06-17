6f88ba3a714b9fd9f03b9f5c899ab8d4e78a2116
carlcnx <carlcnx@outlook.com>
2023-06-17 09:35:40 +0200

    Add StochasticsAlgo

---
+++ Algoloop.Algorithm.CSharp/Algo/StochasticsAlgo.cs
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2023 Capnode AB
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Algoloop.Algorithm.CSharp.Model;
+using QuantConnect;
+using QuantConnect.Algorithm;
+using QuantConnect.Algorithm.Framework.Alphas;
+using QuantConnect.Algorithm.Framework.Risk;
+using QuantConnect.Algorithm.Framework.Selection;
+using QuantConnect.Data.UniverseSelection;
+using QuantConnect.Orders.Fees;
+using QuantConnect.Parameters;
+using QuantConnect.Securities;
+using System.Globalization;
+
+namespace Algoloop.Algorithm.CSharp
+{
+    public class StochasticsAlgo : QCAlgorithm
+    {
+        [Parameter("symbols")]
+        private readonly string _symbols = null;
+
+        [Parameter("security")]
+        private readonly string _security = null;
+
+        [Parameter("resolution")]
+        private readonly string _resolution = null;
+
+        [Parameter("market")]
+        private readonly string _market = null;
+
+        [Parameter("Fee")]
+        private readonly string _fee = "0";
+
+        [Parameter("Period")]
+        private readonly string _period = "0";
+
+        [Parameter("Hold")]
+        private readonly string _hold = "1";
+
+        [Parameter("Slots")]
+        private readonly string _slots = "1";
+
+        [Parameter("Reinvest")]
+        private readonly string _reinvest = "false";
+
+        [Parameter("Rebalance trigger (min)")]
+        private readonly string _rebalance = "0";
+
+        [Parameter("Tracker sma stoploss period1")]
+        private readonly string _trackerPeriod1 = "0";
+
+        [Parameter("Tracker sma stoploss period2")]
+        private readonly string _trackerPeriod2 = "0";
+
+        [Parameter("Tracker range stoploss period")]
+        private readonly string _rangePeriod = "0";
+
+        [Parameter("Market capitalization (M min)")]
+        private readonly string _marketCap = null;
+
+        [Parameter("Revenue growth% (R12 min)")]
+        private readonly string _revenueGrowth = null;
+
+        [Parameter("Revenue trend% (R12 min)")]
+        private readonly string _revenueTrend = null;
+
+        [Parameter("Net income (R12 min)")]
+        private readonly string _netIncome = null;
+
+        [Parameter("Net income (Q min)")]
+        private readonly string _netIncomeQuarter = null;
+
+        [Parameter("Net income growth% (R12 min)")]
+        private readonly string _netIncomeGrowth = null;
+
+        [Parameter("Net income trend% (R12 min)")]
+        private readonly string _netIncomeTrend = null;
+        
+        [Parameter("Net margin% (R12 min)")]
+        private readonly string _netMargin = null;
+
+        [Parameter("Net margin trend% (R12 min)")]
+        private readonly string _netMarginTrend = null;
+
+        [Parameter("Free cash flow margin% (R12 min)")]
+        private readonly string _freeCashFlowMargin = null;
+
+        [Parameter("Return on equity% (R12 min)")]
+        private readonly string _returnOnEquity = null;
+
+        [Parameter("PE ratio (R12 min)")]
+        private readonly string _peRatio = null;
+
+        [Parameter("EP ratio (R12 min)")]
+        private readonly string _epRatio = null;
+        
+        public override void Initialize()
+        {
+            if (string.IsNullOrEmpty(_symbols)) throw new ArgumentNullException(nameof(_symbols));
+            if (string.IsNullOrEmpty(_security)) throw new ArgumentNullException(nameof(_security));
+            if (string.IsNullOrEmpty(_resolution)) throw new ArgumentNullException(nameof(_resolution));
+            if (string.IsNullOrEmpty(_market)) throw new ArgumentNullException(nameof(_market));
+
+            SecurityType securityType = (SecurityType)Enum.Parse(typeof(SecurityType), _security);
+            Resolution resolution = (Resolution)Enum.Parse(typeof(Resolution), _resolution);
+            decimal fee = decimal.Parse(_fee, CultureInfo.InvariantCulture);
+            int period = int.Parse(_period, CultureInfo.InvariantCulture);
+            int hold = int.Parse(_hold, CultureInfo.InvariantCulture);
+            int slots = int.Parse(_slots, CultureInfo.InvariantCulture);
+            bool reinvest = bool.Parse(_reinvest);
+            float rebalance = float.Parse(_rebalance, CultureInfo.InvariantCulture);
+            int trackerPeriod1 = int.Parse(_trackerPeriod1, CultureInfo.InvariantCulture);
+            int trackerPeriod2 = int.Parse(_trackerPeriod2, CultureInfo.InvariantCulture);
+            int rangePeriod = int.Parse(_rangePeriod, CultureInfo.InvariantCulture);
+
+            List<Symbol> symbols = _symbols
+                .Split(';')
+                .Select(x => QuantConnect.Symbol.Create(x, securityType, _market))
+                .ToList();
+
+            EnableAutomaticIndicatorWarmUp = true;
+            UniverseSettings.Resolution = resolution;
+            MarketHoursDatabase.Entry entry = MarketHoursDatabase.GetEntry(_market, (string)null, securityType);
+            SetTimeZone(entry.DataTimeZone);
+            SetUniverseSelection(new ManualUniverseSelectionModel(symbols));
+            SetPortfolioConstruction(new SlotPortfolio(
+                slots: slots,
+                reinvest: reinvest,
+                rebalance: rebalance,
+                trackerPeriod1: trackerPeriod1 >= 0 ? trackerPeriod1 : period,
+                trackerPeriod2: trackerPeriod2 >= 0 ? trackerPeriod2 : period,
+                rangePeriod : rangePeriod));
+            SetExecution(new LimitExecution(slots));
+            SetRiskManagement(new NullRiskManagementModel());
+            SetBenchmark(QuantConnect.Symbol.Create("OMXSPI.ST", securityType, _market));
+            FeeModel feeModel = fee < 1 ? new PercentFeeModel(fee) : new ConstantFeeModel(fee);
+            SetSecurityInitializer(security =>
+            {
+                security.FeeModel = feeModel;
+                security.FillModel = new TouchFill();
+            });
+            SetWarmUp((int)(1.1 * period), Resolution.Daily);
+            SetAlpha(new MultiSignalAlpha(InsightDirection.Up, resolution, hold, symbols,
+                (symbol) => new FundamentalSignal(
+                    this,
+                    symbol,
+                    marketCap: _marketCap,
+                    netIncome: _netIncome,
+                    netIncomeQuarter: _netIncomeQuarter,
+                    netIncomeGrowth: _netIncomeGrowth,
+                    netIncomeTrend: _netIncomeTrend,
+                    revenueGrowth: _revenueGrowth,
+                    revenueTrend: _revenueTrend,
+                    netMargin: _netMargin,
+                    netMarginTrend: _netMarginTrend,
+                    freeCashFlowMargin: _freeCashFlowMargin,
+                    returnOnEquity: _returnOnEquity,
+                    peRatio: _peRatio,
+                    epRatio: _epRatio),
+                (symbol) => new StochasticsSignal(this, symbol, resolution, period)));
+        }
+
+        public override void OnEndOfAlgorithm()
+        {
+            PortfolioConstruction.CreateTargets(this, null);
+        }
+    }
+}
---
+++ Algoloop.Algorithm.CSharp/Signal/StochasticsSignal.cs
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 Capnode AB
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using QuantConnect;
+using QuantConnect.Algorithm;
+using QuantConnect.Data;
+using QuantConnect.Indicators;
+
+namespace Algoloop.Algorithm.CSharp.Model
+{
+    public class StochasticsSignal : ISignal
+    {
+        private readonly Stochastic _sto;
+
+        public StochasticsSignal(QCAlgorithm algorithm, Symbol symbol, Resolution resolution, int period)
+        {
+            if (period > 0)
+            {
+                _sto = algorithm.STO(symbol, period, resolution);
+            }
+        }
+
+
+        public float Update(QCAlgorithm algorithm, BaseData bar)
+        {
+            if (_sto == null) return float.NaN;
+            if (!_sto.IsReady) return 0;
+            decimal score = _sto.StochK.Current.Value;
+            return (float)score;
+        }
+
+        public void Done()
+        {
+        }
+    }
+}
