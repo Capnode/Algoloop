afcf6ecbc4ff3a42fc58c6b59e016149139dd2de
Carl Christerson <carlcnx@outlook.com>
2024-06-08 19:17:20 +0200

    Update LocalDiskMapFileProvider and add new namespaces
    Updated the copyright notice in `LocalDiskMapFileProvider.cs` to include "Modifications Copyright (C) 2022 Capnode AB". Added two new namespaces, `QuantConnect.Util` and `QuantConnect.Configuration`. Initialized a new `_dataProvider` field in the `LocalDiskMapFileProvider` constructor, which is set to an instance of `IDataProvider` retrieved from the `Composer` class using the type name specified in the configuration under the key "data-provider", with "DefaultDataProvider" as the default type name if no such configuration exists.

--- Common/Data/Auxiliary/LocalDiskMapFileProvider.cs
+++ Common/Data/Auxiliary/LocalDiskMapFileProvider.cs
@@ -1,6 +1,7 @@
 /*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * Modifications Copyright (C) 2022 Capnode AB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +20,8 @@ using System.Threading;
 using QuantConnect.Logging;
 using QuantConnect.Interfaces;
 using System.Collections.Concurrent;
+using QuantConnect.Util;
+using QuantConnect.Configuration;
 
 namespace QuantConnect.Data.Auxiliary
 {
@@ -38,6 +41,8 @@ namespace QuantConnect.Data.Auxiliary
         public LocalDiskMapFileProvider()
         {
             _cache = new ConcurrentDictionary<AuxiliaryDataKey, MapFileResolver>();
+            _dataProvider = Composer.Instance.GetExportedValueByTypeName<IDataProvider>(
+                Config.Get("data-provider", "DefaultDataProvider"));
         }
 
         /// <summary>
